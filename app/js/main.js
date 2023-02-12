e58958a7021ed6d62cede29cecbebb03c56ed11e
Sassoricun <galactix.69vk256.1@gmail.com>
2023-02-12 23:20:03 +0200

    added shop page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,29 @@
 $(function () {
 
+  $('.star').rateYo({
+    starWidth: "12px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    spacing: "5px",
+    readOnly: true,
+
+  });
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    step: 0.01,
+    prettify_enabled: true,
+    prettify_separator: ".",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
+
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
