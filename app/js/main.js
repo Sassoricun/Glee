a43a098abd174dfaac36f52ccdc72126b6db7e2b
Sassoricun <galactix.69vk256.1@gmail.com>
2023-02-25 00:17:16 +0200

    added product details

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,42 @@
 $(function () {
+  $('.shop-items-product').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    draggable: false,
+  });
+
+
+
+
+  $('.product-tabs__top-item').on('click', function (e) {
+    e.preventDefault();
+    $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+    $(this).addClass('product-tabs__top-item--active');
+
+    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+    $($(this).attr('href')).addClass('product-tabs__content-item--active');
+  });
+
+
+  $('.product-details-slide__thumb').slick({
+    asNavFor: '.product-details-slide__big',
+    focusOnSelect: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false,
+    arrows: false,
+  });
+  $('.product-details-slide__big').slick({
+    asNavFor: '.product-details-slide__thumb',
+    draggable: false,
+    arrows: false,
+    fade: true,
+  });
 
   $('.star').rateYo({
     starWidth: "12px",
@@ -37,5 +75,5 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 2000
   })
-
+  $('.product-details-item__num').styler();
 });
