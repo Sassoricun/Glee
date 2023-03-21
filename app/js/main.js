c3242808c2d0a97d2878bf90b8f49bec41ee8dfb
Sassoricun <galactix.69vk256.1@gmail.com>
2023-03-21 21:56:22 +0200

    added contacts page

--- app/js/main.js
+++ app/js/main.js
@@ -80,4 +80,15 @@ $(function () {
     autoplaySpeed: 2000
   })
   $('.product-details-item__num').styler();
+
+
+
+  let map;
+
+  function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 8,
+    });
+  }
 });
